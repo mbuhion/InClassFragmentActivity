0019be1475ccaf9d2e6b454f6e082581169166bc
mbuhion <tur49580@temple.edu>
2026-03-12 11:41:33 -0400

    Implemented ImageDisplayFragment and FragmentContainerView

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -10,10 +10,14 @@ class MainActivity : AppCompatActivity() {
 
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
-        val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
+        val imageArray = IntArray(typedArray.length()) { typedArray.getResourceId(it, 0) }
         typedArray.recycle()
 
-        // Attach an instance of ImageDisplayFragment using factory method
-
+        if (savedInstanceState == null) {
+            val fragment = ImageDisplayFragment.newInstance(imageArray)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.image_fragment_container, fragment)
+                .commit()
+        }
     }
 }
